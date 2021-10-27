362d24ffcd1e02ad0594da3329c23315bf76bc65
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-10-27 21:34:17 +0500

    Add files via upload
    задание 4

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace КомароваЗадание4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            Console.WriteLine(Math.Round(value: x = Metod, digits: 3));
+            Console.ReadKey();
+        }
+        static double Metod
+        {
+            get
+            {
+                double x;
+                double q = Math.Sqrt(3);
+                double w = Math.Sqrt(2);
+                double e = Math.Sqrt(5);
+                double r = Math.Sqrt(11);
+                double t = Math.Sqrt(7);
+                return x = ((q - w) / (q + w) + (e - q) / (e + q) + (r - t) / (r + t));
+            }
+        }
+    }
+}
