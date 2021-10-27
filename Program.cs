03c5010646e85ff81debba7d011d00f6a8bfcf47
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-10-27 21:35:55 +0500

    Add files via upload
    задание 5, номер 1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace КомароваЗадание5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            text = text.Replace("a", "4");
+            text = text.Replace("e", "3");
+            text = text.Replace("i", "!");
+            text = text.Replace("q", "9");
+            text = text.Replace("b", "8");
+            text = text.Replace("f", "|=");
+            text = text.Replace("j", ")");
+            text = text.Replace("r", "|2");
+            text = text.Replace("z", "2");
+            text = text.Replace("c", "(");
+            text = text.Replace("g", "6");
+            text = text.Replace("k", "|<");
+            text = text.Replace("o", "0");
+            text = text.Replace("s", "5");
+            text = text.Replace("d", "|)");
+            text = text.Replace("h", "|-|");
+            text = text.Replace("l", "1");
+            text = text.Replace("p", "|>");
+            text = text.Replace("t", "7");
+            text = text.Replace("x", "><");
+            Console.WriteLine(text);
+
+        }
+    }
+}
