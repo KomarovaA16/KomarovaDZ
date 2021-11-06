0ca2104c3faf358f2bc7d2d04fb9039d96f14231
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-11-06 23:25:30 +0500

    Add files via upload
    переделанное задание 4

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace КомароваЗадание4
+{
+    class Program
+    {
+
+        static double Metod(double q,double w)
+        {
+            return (Math.Sqrt(q)-Math.Sqrt(w))/(Math.Sqrt(q) + Math.Sqrt(w)); 
+
+
+        }
+
+            static void Main(string[] args)
+        {
+            double x = Metod(3,2)+Metod(5,3)+Metod(11,7);
+            Console.WriteLine($"x = {x:F3}");
+        }
+
+    }
+}
+        
