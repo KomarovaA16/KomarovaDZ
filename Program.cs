8bf034c7275c438970c88c379c6d6e7b84ef1464
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-11-17 14:53:02 +0500

    Add files via upload
    задание 6.1

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace КомароваЗадание6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значения k,m,n : ");
+            var k = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var n = int.Parse(Console.ReadLine());
+            if (k > 15)
+            {
+                if (m > 15)
+                {
+                    Console.WriteLine("m не удовлетворяет условию, F=0");
+                }
+                else
+                {
+                    if (n > 15)
+                    {
+                        Console.WriteLine("n не удовлетворяет условию, F=0");
+                    }
+                    else Console.WriteLine("F=1");
+                }
+            }
+            else
+            {
+                if (m > 15)
+                {
+                    if (n > 15)
+                    {
+                        Console.WriteLine("n не удовлетворяет условию, F=0");
+                    }
+                    else Console.WriteLine("F=1");
+                }
+                else
+                {
+                    if (n > 15)
+                    {
+                        Console.WriteLine("F=1");
+                    }
+                    else Console.WriteLine("n не удовлетворяет условию, F=0");
+                }
+
+            }
+        }
+    }
+}
