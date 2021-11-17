7c0a7d89400d8dc793d541616989a2f76d74ab7a
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-11-17 15:36:04 +0500

    Add files via upload
    задание 7

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace КомароваЗадание7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение Х : ");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Значение функции f(x) = {Metod(x)}");
+        }
+        static double Metod (double x)
+        {
+            double f;
+            if (x < -1)
+                f = 1;
+            else
+            {
+                if (x <= 1)
+                    f = Math.Pow(x, 2);
+                else
+                    f = 2 - x;
+            }
+
+            return f;
+        }
+    }
+}
