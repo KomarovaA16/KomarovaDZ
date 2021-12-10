26b76e873f9a8a5cf203e6a32d0a62d0104203de
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-12-10 11:37:54 +0500

    Add files via upload
    Задание 8.1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace КомароваЗадание8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите границу перевода");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение шага");
+            var h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Таблица перевода из дюймов в сантиметры :");
+            for (var i = 1; i < a;)
+            {
+                var x = 2.54*i;
+                Console.WriteLine($"{i} д. = {x} см");
+                i= (int)(i + h);
+            }
+        }
+    }
+}
+
