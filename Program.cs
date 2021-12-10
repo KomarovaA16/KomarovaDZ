6f7d141cb19d8a63f3b134c9d04ca03af1644939
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-12-10 11:45:32 +0500

    Add files via upload
    Задание 8.6

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace КомароваЗадание8._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите границу дружественных чисел");
+            var n = double.Parse(Console.ReadLine());
+            var result = 1;
+            var sdel = 0;
+            for (int i=2;i<n;i++)
+            {
+                if (n % i == 0)
+                    result = result + i;
+                sdel = result;
+            }
+            for (int i =1;i<(n-1);i++)
+            {
+                for (var j = (i + 1); j < n; j++)
+                    if ((sdel == j) && (sdel == i) && (j > i))
+                    {
+                        Console.WriteLine($"Два дружественных числа:{i},{j}");
+                    }
+                    else Console.WriteLine("Дружественных чисел до указанной границы нет");
+            }
+        }
+    }
+}
