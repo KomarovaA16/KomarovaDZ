8e03c23a45b38c8a42e341bb16e86c197c4591f2
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2021-12-10 11:44:10 +0500

    Add files via upload
    Задание 8.5

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace КомароваЗадание8._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите дистанцию в первый день :");
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите процент, на который каждый день увеличивается дистанция :");
+            double m = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите нужную конечную дистанцию :");
+            double k = double.Parse(Console.ReadLine());
+            var d = 1;
+            while (n<=k)
+            {
+                n = n +(n*m*0.01);
+                d++;
+            }
+            Console.WriteLine($"Эту дистанцию можно пробежать за {d} д.");
+        }
+    }
+}
