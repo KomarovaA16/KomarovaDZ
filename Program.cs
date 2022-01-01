e3a0a2e523ab0aed7768755724d8fb7aaef8b63b
Комарова Александра <91051712+KomarovaA16@users.noreply.github.com>
2022-01-01 17:45:34 +0500

    Add files via upload
    переделанное задание 6.2

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace КомароваЗадание6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координату Х : ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату Y : ");
+            double y = double.Parse(Console.ReadLine());
+            if (Metod(y) == 1)
+            Console.WriteLine("Точка принадлежит выбранному участку");
+            else
+                Console.WriteLine("Точка не принадлежит выбранному участку");
+        }
+        static double Metod (double y)
+        {
+            int F;
+            if (y>1)
+            {
+                F=1;
+            }
+            else
+            {
+                if (y < -3)
+                {
+                    F=1;
+                }
+                F=0;
+            }
+            return F;
+        }
+    }
+}
